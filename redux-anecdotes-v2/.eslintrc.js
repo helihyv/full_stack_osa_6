e768718a7cc24eb397bfc80b6d8ac19901074ad6
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-10-18 11:07:16 +0300

    Tehtävä 6.1
    Valmis.

---
+++ redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals":true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint",
+
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "jest"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-console": 0,
+        "react/prop-types": 0
+    }
+};
