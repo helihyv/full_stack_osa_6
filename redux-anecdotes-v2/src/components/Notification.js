2d4e42e8a0ab578b4e45fb6abed0287e862ad6ea
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-10-18 15:29:49 +0300

    Tehtävä 6.3
    Valmis.

--- redux-anecdotes-v2/src/components/Notification.js
+++ redux-anecdotes-v2/src/components/Notification.js
@@ -7,9 +7,10 @@ class Notification extends React.Component {
       padding: 10,
       borderWidth: 1
     }
+
     return (
-      <div style={style}>
-        render here notification...
+      <div style={style}> 
+        {this.props.store.getState().notification}
       </div>
     )
   }
---
+++ redux-anecdotes-v2/src/reducers/notificationReducer.js
@@ -0,0 +1,11 @@
+const initialState =  'Welcome!'
+
+const notificationReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+  default:
+    return state
+  }
+}
+
+export default notificationReducer
